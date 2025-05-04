ce926c82d1feaf186c2ac9d7f8dcd1ee51db8d01
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-04 21:28:34 +0900

    theme

--- app/src/main/java/com/example/news/ui/theme/Theme.kt
+++ app/src/main/java/com/example/news/ui/theme/Theme.kt
@@ -1,58 +1,81 @@
 package com.example.news.ui.theme
 
-import android.app.Activity
-import android.os.Build
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.news.R
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+private val LocalColors = compositionLocalOf<Colors> {
+    error("No colors provided!")
+}
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+private val LocalCustomTypography = compositionLocalOf<CustomTypography> {
+    error("No typography provided!")
+}
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+val fontFamily = FontFamily(
+    Font(R.font.pretendard_black, FontWeight.Black),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),
+    Font(R.font.pretendard_light, FontWeight.Light),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_thin, FontWeight.Thin),
+    Font(R.font.pretendard_extralight, FontWeight.ExtraLight),
 )
 
 @Composable
 fun NewsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    colors: Colors = if (darkTheme) {
+        Colors.defaultDarkColors()
+    } else {
+        Colors.defaultLightColors()
+    },
+    background : Background = Background.defaultBackground(darkTheme),
+    customTypography: CustomTypography = CustomTypography.defaultCustomTypography(fontFamily),
+    content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    CompositionLocalProvider(
+        LocalColors provides colors,
+        LocalBackgroundTheme provides background,
+        LocalCustomTypography provides customTypography
+    ) {
+        Box(
+            modifier = Modifier
+                .background(background.color)
+                .semantics { testTagsAsResourceId = true },
+        ) {
+            content()
         }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
     }
+}
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+public object NewsTheme {
+
+    public val colors: Colors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColors.current
+
+    public val background: Background
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalBackgroundTheme.current
+
+    public val typography: CustomTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalCustomTypography.current
 }
