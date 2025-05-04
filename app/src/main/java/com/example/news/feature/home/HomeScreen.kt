a64a9d4df14aa50c58953501a16a5bd6db75f4d7
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-05 02:16:58 +0900

    home

--- app/src/main/java/com/example/news/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/news/feature/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.news.feature.home
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,16 +9,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.example.news.R
 import com.example.news.core.Dimens
 import com.example.news.data.model.News
 import com.example.news.ui.theme.NewsTheme
@@ -72,12 +75,73 @@ private fun HomeContent(
 
 
 @Composable
-private fun NewsCard(
+private fun NewsList(
     uiState: HomeUiState,
-    news: List<News>,
+    newsList: List<News>,
     fetchNextNews: () -> Unit,
 ){
-    Column {
+    Box(modifier = Modifier.fillMaxSize()) {
+        val threadHold = 8
+        LazyColumn(
+            modifier = Modifier.padding(Dimens.verticalPadding),
+            verticalArrangement = Arrangement.spacedBy(Dimens.gapMedium),
+            state = rememberLazyListState(),
+        ) {
+            stickyHeader {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text(
+                        text = "핫 뉴스 \uD83D\uDE80",
+                        style = NewsTheme.typography.header,
+                        color = NewsTheme.colors.textPrimary
+                    )
+                    Row(
+                        modifier = Modifier.clickable{},
+                    ) {
+                        Text(
+                            text = "더보기",
+                            style = NewsTheme.typography.more,
+                            color = NewsTheme.colors.textThird,
+                        )
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.chevron_right_small),
+                            contentDescription = null,
+                            tint = NewsTheme.colors.textThird,
+                        )
+                    }
+                }
+            }
+            itemsIndexed(items = newsList, key = { _, news -> news.id }) { index, news ->
+                if ((index + threadHold) >= newsList.size && uiState != HomeUiState.Loading) {
+                    fetchNextNews()
+                }
+                NewsCard(
+                    news = news
+                )
+            }
+        }
+
+        if (uiState == HomeUiState.Loading) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(Dimens.gapLarge),
+                strokeWidth = Dimens.border
+            )
+        }
+    }
+}
+
+@Composable
+private fun NewsCard(
+    news: News
+){
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
 
     }
 }
