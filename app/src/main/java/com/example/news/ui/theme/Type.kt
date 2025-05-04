841595f187e5a897fd6e2c050d7726a89ce5e209
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-04 21:31:21 +0900

    typo

--- app/src/main/java/com/example/news/ui/theme/Type.kt
+++ app/src/main/java/com/example/news/ui/theme/Type.kt
@@ -2,14 +2,10 @@ package com.example.news.ui.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.ProvidableCompositionLocal
-import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.news.R
 
 @Immutable
 public data class CustomTypography(
