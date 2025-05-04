d6f34398bff3fe3082c8909e76bf7903c17c836d
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-05 02:04:01 +0900

    dimens

---
+++ app/src/main/java/com/example/news/core/Dimens.kt
@@ -0,0 +1,13 @@
+package com.example.news.core
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val horizontalPadding = 20.dp
+    val verticalPadding = 10.dp
+    val gapLarge = 20.dp
+    val gapMedium = 10.dp
+    val gapSmall = 4.dp
+    val cornerRadius = 12.dp
+    val border = 1.dp
+}
