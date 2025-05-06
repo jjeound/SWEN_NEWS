592cc46426bad20138bcf6c7dda6fb99c5033c33
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-06 23:15:09 +0900

    navigation

--- app/src/main/java/com/example/news/navigation/Navigation.kt
+++ app/src/main/java/com/example/news/navigation/Navigation.kt
@@ -4,8 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import com.example.news.feature.detail.DetailScreen
 import com.example.news.feature.home.HomeScreen
+import com.example.news.feature.more.MoreScreen
 
 @Composable
 fun Navigation(navHostController: NavHostController){
@@ -14,7 +16,20 @@ fun Navigation(navHostController: NavHostController){
         startDestination = Screen.Home,
     ) {
         composable<Screen.Home>{
-            HomeScreen()
+            HomeScreen(
+                navigateToMore = { isFirst ->
+                    navHostController.navigate(Screen.More(isFirst))
+                },
+            )
+        }
+        composable<Screen.More>{
+            val args = it.toRoute<Screen.More>()
+            MoreScreen(
+                isFirst = args.isFirst,
+                navigateToDetail = { newsId ->
+                    navHostController.navigate(Screen.Detail(newsId))
+                }
+            )
         }
         composable<Screen.Detail>{
             DetailScreen()
