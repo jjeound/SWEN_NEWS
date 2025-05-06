f1da314f1b2aed73e27b60a30ae312110e5b7c4a
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-06 23:15:44 +0900

    model

---
+++ app/src/main/java/com/example/news/data/model/NewsDetail.kt
@@ -0,0 +1,40 @@
+package com.example.news.data.model
+
+data class NewsDetail(
+    val id: Long,
+    val title: String,
+    val thumbnail: String,
+    val left: Left,
+    val right: Right,
+    val center: Center,
+    val totalCount: Int,
+    val mediaOp: List<String>,
+    val originalSource: List<Source>
+)
+
+data class Left(
+    val summary: List<String>,
+    val keywords: List<Keyword>,
+    val media: List<String>,
+)
+data class Right(
+    val summary: List<String>,
+    val keywords: List<Keyword>,
+    val media: List<String>,
+)
+data class Center(
+    val summary: List<String>,
+    val keywords: List<Keyword>,
+    val media: List<String>,
+)
+data class Keyword(
+    val keyword: String,
+    val count: Int
+)
+
+data class Source(
+    val bias: Int,
+    val media: String,
+    val title: String,
+    val url: String
+)
