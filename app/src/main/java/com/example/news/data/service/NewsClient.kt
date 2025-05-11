669d6db6a0486c79bb46d420e31506dda9b6b188
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-11 16:54:50 +0900

    service

--- app/src/main/java/com/example/news/data/service/NewsClient.kt
+++ app/src/main/java/com/example/news/data/service/NewsClient.kt
@@ -1,17 +1,38 @@
 package com.example.news.data.service
 
 import com.example.news.data.dto.ApiResponse
-import com.example.news.data.dto.HotNews
+import com.example.news.data.dto.HotNewsResponse
+import com.example.news.data.dto.NewsInfo
 import javax.inject.Inject
 
 class NewsClient @Inject constructor(
     private val newsService: NewsService
 ){
-    suspend fun getHotNewsList(page: Int): ApiResponse<HotNews> =
+    suspend fun getHotNewsList(page: Int): ApiResponse<HotNewsResponse> =
         newsService.getHotNewsList(
             limit = PAGING_SIZE,
             page = page * PAGING_SIZE + 1,
         )
+
+    suspend fun getLatestNewsList(page: Int): ApiResponse<HotNewsResponse> =
+        newsService.getLatestNewsList(
+            limit = PAGING_SIZE,
+            page = page * PAGING_SIZE + 1,
+        )
+    suspend fun getCategoryHotNewsList(category: String, page: Int): ApiResponse<HotNewsResponse> =
+        newsService.getCategoryHotNewsList(
+            category = category,
+            limit = PAGING_SIZE,
+            page = page * PAGING_SIZE + 1,
+        )
+    suspend fun getCategoryLatestNewsList(category: String, page: Int): ApiResponse<HotNewsResponse> =
+        newsService.getCategoryLatestNewsList(
+            category = category,
+            limit = PAGING_SIZE,
+            page = page * PAGING_SIZE + 1,
+        )
+    suspend fun getNewsDetail(id: String): ApiResponse<NewsInfo> =
+        newsService.getNewsDetail(id = id)
 
     companion object {
         private const val PAGING_SIZE = 10
--- app/src/main/java/com/example/news/data/service/NewsService.kt
+++ app/src/main/java/com/example/news/data/service/NewsService.kt
@@ -1,9 +1,10 @@
 package com.example.news.data.service
 
 import com.example.news.data.dto.ApiResponse
-import com.example.news.data.dto.Cluster
-import com.example.news.data.dto.HotNews
+import com.example.news.data.dto.HotNewsResponse
+import com.example.news.data.dto.NewsInfo
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NewsService {
@@ -11,5 +12,30 @@ interface NewsService {
     suspend fun getHotNewsList(
         @Query("limit") limit: Int = 10,
         @Query("page") page: Int = 1,
-    ): ApiResponse<HotNews>
+    ): ApiResponse<HotNewsResponse>
+
+    @GET("/clusters/latest")
+    suspend fun getLatestNewsList(
+        @Query("limit") limit: Int = 10,
+        @Query("page") page: Int = 1,
+    ): ApiResponse<HotNewsResponse>
+
+    @GET("/clusters/hot/{category}")
+    suspend fun getCategoryHotNewsList(
+        @Path("category") category: String,
+        @Query("limit") limit: Int = 10,
+        @Query("page") page: Int = 1,
+    ): ApiResponse<HotNewsResponse>
+
+    @GET("/clusters/latest/{category}")
+    suspend fun getCategoryLatestNewsList(
+        @Path("category") category: String,
+        @Query("limit") limit: Int = 10,
+        @Query("page") page: Int = 1,
+    ): ApiResponse<HotNewsResponse>
+
+    @GET("/clusters/{id}")
+    suspend fun getNewsDetail(
+        @Path("id") id: String,
+    ): ApiResponse<NewsInfo>
 }
