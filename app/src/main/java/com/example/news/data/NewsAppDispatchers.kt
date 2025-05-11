882b4a937c94b545df0e61c17634977df5ef9380
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-11 16:55:23 +0900

    dispatchers

---
+++ app/src/main/java/com/example/news/data/NewsAppDispatchers.kt
@@ -0,0 +1,12 @@
+package com.example.news.data
+
+import javax.inject.Qualifier
+import kotlin.annotation.AnnotationRetention.RUNTIME
+
+@Qualifier
+@Retention(RUNTIME)
+annotation class Dispatcher(val newsAppDispatchers: NewsAppDispatchers)
+
+enum class NewsAppDispatchers {
+    IO,
+}
