e188bed963383c5c60d47872203975f7065eec3f
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-12 21:57:59 +0900

    main

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -4,19 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.example.news.navigation.AppComposeNavigator
+import com.example.news.navigation.LocalComposeNavigator
+import com.example.news.navigation.NewsNavHost
+import com.example.news.navigation.Screen
 import com.example.news.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    internal lateinit var composeNavigator: AppComposeNavigator<Screen>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         installSplashScreen()
         setContent {
-            NewsTheme {
-                MainScreen()
+            CompositionLocalProvider(
+                LocalComposeNavigator provides composeNavigator,
+            ) {
+                MainScreen(composeNavigator = composeNavigator)
             }
         }
     }
--- app/src/main/java/com/example/news/MainScreen.kt
+++ app/src/main/java/com/example/news/MainScreen.kt
@@ -1,11 +1,22 @@
 package com.example.news
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.rememberNavController
-import com.example.news.navigation.Navigation
+import com.example.news.navigation.AppComposeNavigator
+import com.example.news.navigation.NewsNavHost
+import com.example.news.navigation.Screen
+import com.example.news.ui.theme.NewsTheme
 
 @Composable
-fun MainScreen(){
-    val navHostController = rememberNavController()
-    Navigation(navHostController)
+fun MainScreen(composeNavigator: AppComposeNavigator<Screen>) {
+    NewsTheme {
+        val navHostController = rememberNavController()
+
+        LaunchedEffect(Unit) {
+            composeNavigator.handleNavigationCommands(navHostController)
+        }
+
+        NewsNavHost(navHostController = navHostController)
+    }
 }
