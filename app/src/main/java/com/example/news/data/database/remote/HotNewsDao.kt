ca6a3e363677725a21a3dced29e4039231fddb94
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-15 21:33:49 +0900

    dao

---
+++ app/src/main/java/com/example/news/data/database/remote/HotNewsDao.kt
@@ -0,0 +1,24 @@
+package com.example.news.data.database.remote
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.news.data.database.entity.HotNewsEntity
+
+@Dao
+interface HotNewsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNewsList(newsList: List<HotNewsEntity>)
+
+    @Query("SELECT * FROM hotnewsentity WHERE page = :page_")
+    suspend fun getNewsList(page_: Int): List<HotNewsEntity>
+
+    @Query("SELECT * FROM hotnewsentity")
+    fun getNewsListPaged(): PagingSource<Int, HotNewsEntity>
+
+    @Query("DELETE FROM hotnewsentity")
+    suspend fun clearAll()
+}
---
+++ app/src/main/java/com/example/news/data/database/remote/LatestNewsDao.kt
@@ -0,0 +1,24 @@
+package com.example.news.data.database.remote
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.news.data.database.entity.LatestNewsEntity
+
+@Dao
+interface LatestNewsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNewsList(newsList: List<LatestNewsEntity>)
+
+    @Query("SELECT * FROM latestnewsentity WHERE page = :page_")
+    suspend fun getNewsList(page_: Int): List<LatestNewsEntity>
+
+    @Query("SELECT * FROM latestnewsentity")
+    fun getNewsListPaged(): PagingSource<Int, LatestNewsEntity>
+
+    @Query("DELETE FROM latestnewsentity")
+    suspend fun clearAll()
+}
