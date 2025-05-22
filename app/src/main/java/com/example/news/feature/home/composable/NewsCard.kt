6f6e2cc7d3010f9046bdcafe671f72ba3a20aadc
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-23 01:14:52 +0900

    news

--- app/src/main/java/com/example/news/feature/home/composable/NewsCard.kt
+++ app/src/main/java/com/example/news/feature/home/composable/NewsCard.kt
@@ -1,9 +1,7 @@
 package com.example.news.feature.home.composable
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
