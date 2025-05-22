4d5e3f6499a15748b763522456912d553f7fe304
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-23 01:14:31 +0900

    news

--- app/src/main/java/com/example/news/feature/home/composable/NewsCard.kt
+++ app/src/main/java/com/example/news/feature/home/composable/NewsCard.kt
@@ -3,6 +3,7 @@ package com.example.news.feature.home.composable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,17 +11,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import com.example.news.R
 import com.example.news.core.Dimens
 import com.example.news.data.model.News
 import com.example.news.navigation.Screen
@@ -50,11 +51,17 @@ fun NewsCard(
                 modifier = Modifier.size(92.dp)
             )
         }else{
-            Image(
-                imageVector = ImageVector.vectorResource(R.drawable.ic_launcher_foreground),
-                contentDescription = news.title,
-                modifier = Modifier.size(92.dp)
-            )
+            Card(
+                modifier = Modifier.size(92.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = NewsTheme.colors.center
+                ),
+                shape = RoundedCornerShape(Dimens.gapSmall),
+            ) {
+                Spacer(
+                    modifier = Modifier.size(92.dp)
+                )
+            }
         }
         Column(
             verticalArrangement = Arrangement.spacedBy(Dimens.gapSmall)
