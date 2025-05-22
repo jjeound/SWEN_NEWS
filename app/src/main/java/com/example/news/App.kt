fdfde71f287db1a8d4c5e68fe254fb2db703859a
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-05-23 00:43:00 +0900

    refactoring

--- app/src/main/java/com/example/news/App.kt
+++ app/src/main/java/com/example/news/App.kt
@@ -1,12 +1,6 @@
 package com.example.news
 
 import android.app.Application
-import coil.ImageLoader
-import coil.ImageLoaderFactory
-import coil.disk.DiskCache
-import coil.memory.MemoryCache
-import coil.request.CachePolicy
-import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
